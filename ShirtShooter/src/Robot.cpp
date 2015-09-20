3aeb79e48c9885d99ec3cb5013b610e389a45a94
Aftershock Development Team <jpt263@outlook.com>
2015-09-20 19:12:19 -0400

    ShirtShooter PCM toggles ports 0&1, toggled by Joystick A button.

--- ShirtShooter/src/Robot.cpp
+++ ShirtShooter/src/Robot.cpp
@@ -1,38 +1,40 @@
 #include "WPILib.h"
+#include "Utilities.h"
 
 #include <DoubleSolenoid.h>
+
 #define PIVOT_PISTON_FORWARD_CHANNEL 0
 #define PIVOT_PISTON_REVERSE_CHANNEL 1
 
+#define GAMEPAD_BUTTON_A 1
+#define GAMEPAD_BUTTON_B 2
+#define GAMEPAD_BUTTON_X 3
+#define GAMEPAD_BUTTON_Y 4
+#define GAMEPAD_LEFT_BUMPER 5
+#define GAMEPAD_RIGHT_BUMBER 6
+#define GAMEPAD_BUTTON_LEFT 7
+#define GAMEPAD_BUTTON_RIGHT 8
 
-/**
- * This is a demo program showing the use of the RobotDrive class.
- * The SampleRobot class is the base of a robot application that will automatically call your
- * Autonomous and OperatorControl methods at the right time as controlled by the switches on
- * the driver station or the field controls.
- *
- * WARNING: While it may look like a good choice to use for your code if you're inexperienced,
- * don't. Unless you know what you are doing, complex code will be much more difficult under
- * this system. Use IterativeRobot or Command-Based instead if you're new.
- */
 class Robot: public SampleRobot
 {
 	RobotDrive myRobot; // robot drive system
-	Joystick stick; // only joystick
+	Joystick *gamepad;
 	Timer HeartbeatTimer;
 	const double HeartbeatTickDurationInSeconds = 5.0;
 	long lifetimeTickCounter = 0;
+	Utilities *theUtilities;
 	DoubleSolenoid *theDoubleSolenoid;
 
 public:
 	Robot() :
-			myRobot(0, 1, 2, 3),	// these must be initialized in the same order
-			stick(0)		// as they are declared above.
+			myRobot(0, 1, 2, 3)	// these must be initialized in the same order
 	{
 		myRobot.SetExpiration(0.1);
 		HeartbeatTimer.Reset();
 		HeartbeatTimer.Start();
 		printf("RoboRIO Initialized.\n");
+		theUtilities = new Utilities();
+		gamepad = new Joystick(0);
 		theDoubleSolenoid = new DoubleSolenoid(PIVOT_PISTON_FORWARD_CHANNEL,PIVOT_PISTON_REVERSE_CHANNEL);
 		theDoubleSolenoid->Set(DoubleSolenoid::kReverse);  // Retract it so robot can move around.
 	}
@@ -41,11 +43,9 @@ public:
 	{
 		theDoubleSolenoid->Set(DoubleSolenoid::kReverse); // Retract it so robot can move and be moved around.
 		delete theDoubleSolenoid;
+		delete theUtilities;
 	}
 
-	/**
-	 * Drive left & right motors for 2 seconds then stop
-	 */
 	void Autonomous()
 	{
 		myRobot.SetSafetyEnabled(false);
@@ -54,7 +54,7 @@ public:
 			ProcessHeartbeat();
 
 			//myRobot.Drive(-0.5, 0.0); 	// drive forwards half speed
-			Wait(1.0); 				//    for 2 seconds
+			Wait(0.005); 				//    for 2 seconds
 			//myRobot.Drive(0.0, 0.0); 	// stop robot
 		}
 	}
@@ -68,9 +68,10 @@ public:
 		myRobot.SetSafetyEnabled(true);
 		while (IsOperatorControl() && IsEnabled())
 		{
+			CommandPivotPistonPosition();
 			ProcessHeartbeat();
 
-			myRobot.ArcadeDrive(stick); // drive with arcade style (use right stick)
+			myRobot.ArcadeDrive(gamepad); // drive with arcade style (use right stick)
 			Wait(0.005);				// wait for a motor update time
 		}
 	}
@@ -88,10 +89,9 @@ private:
     {
 		if (HeartbeatTimer.HasPeriodPassed(HeartbeatTickDurationInSeconds))
 		{
-			printf("Heartbeat tick %ld (every %f seconds).  DoubleSolenoid position(%d)\n",
+			printf("Heartbeat tick %ld (every %f seconds).\n",
 					lifetimeTickCounter++,
-					HeartbeatTickDurationInSeconds,
-					TogglePiston());
+					HeartbeatTickDurationInSeconds);
 			HeartbeatTimer.Reset();
 			HeartbeatTimer.Start();
 		}
@@ -108,6 +108,16 @@ private:
 
 	}
 
+	void CommandPivotPistonPosition() {
+	    if (theUtilities->GetJoystickButton(GAMEPAD_BUTTON_A, gamepad)) {
+	    	if (theDoubleSolenoid->Get() == DoubleSolenoid::kReverse) {
+	    		theDoubleSolenoid->Set(DoubleSolenoid::kForward); // Deploy it so its anchored, and robot can pivot arround it.
+	    	} else {
+	    		theDoubleSolenoid->Set(DoubleSolenoid::kReverse); // Retract it so robot can move around.
+	    	}
+			printf("PistonPositionToggled (%d)\n", TogglePiston());
+	    }
+	}
 };
 
 START_ROBOT_CLASS(Robot);
